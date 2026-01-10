36017f2b61089934b5b6a05637183556d073ebe1
axtrough <lolmyfriend14@gmail.com>
2026-01-10 06:03:47 +0100

    i dont even need a registry duh

--- src/main/java/net/raccoon/will/structura/Structura.java
+++ src/main/java/net/raccoon/will/structura/Structura.java
@@ -1,8 +1,6 @@
 package net.raccoon.will.structura;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
@@ -12,22 +10,13 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.NewRegistryEvent;
-import net.neoforged.neoforge.registries.RegistryBuilder;
-import net.raccoon.will.structura.api.gui.element.GuiElement;
 import org.slf4j.Logger;
 
 @Mod(Structura.MODID)
 public class Structura {
     public static final String MODID = "structura";
     private static final Logger LOGGER = LogUtils.getLogger();
-
-    public static final ResourceKey<Registry<GuiElement>> GUI_KEY = ResourceKey.createRegistryKey(Structura.resLoc("gui_element"));
-    public static final Registry<GuiElement> GUI_ELEMENTS = new RegistryBuilder<>(GUI_KEY)
-            .sync(true)
-            .create();
 
     public static ResourceLocation resLoc(String name) {
         return ResourceLocation.fromNamespaceAndPath(Structura.MODID, name);
@@ -38,11 +27,6 @@ public class Structura {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-    }
-
-    @SubscribeEvent
-    public static void registerRegistries(NewRegistryEvent event) {
-        event.register(Structura.GUI_ELEMENTS);
     }
 
     @SubscribeEvent
